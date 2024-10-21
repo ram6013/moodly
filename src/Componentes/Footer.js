418c1d1a415aba2465aba3acde11d890c4e56b49
Alberto Ruiz Fuente <albertoruizfuente@gmail.com>
2024-10-21 19:54:26 +0200

    about us + footer

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLanguage } from '../LanguageContext'; 
 
 function Footer() {
     const { isLanguageSpanish, toggleLanguageToSpanish, toggleLanguageToEnglish } = useLanguage();
@@ -9,21 +11,19 @@ function Footer() {
     };
     
     return (
-        <footer className="footer">
-            <div className="footer-content">
-                <div className="footer-left">
+            <footer className="flex flex-row justify-around mt-10">
+                <div className="flex">
                     <p>&copy; {new Date().getFullYear()} Moodly. All rights reserved.</p>
                 </div>
-                <div className="footer-links">
-                    <button onClick={() => handleNavigate("")} className="text-white text-2xl ml-2 transition-transform duration-200 hover:scale-110 font-mochiy">
+                <div className="flex ">
+                    <button onClick={() => handleNavigate("")} className="text-gray-400 text-xs ml-2 transition-transform duration-200 hover:scale-110 font-mochiy ">
                         Privacy Policy
                     </button>
-                    <button onClick={() => handleNavigate("")} className="text-white text-2xl ml-2 transition-transform duration-200 hover:scale-110 font-mochiy">
-                        cookie Policy
+                    <button onClick={() => handleNavigate("")} className="text-gray-400 text-xs ml-2 transition-transform duration-200 hover:scale-110 font-mochiy">
+                        Cookie Policy
                     </button>
                 </div>
-            </div>
-        </footer>
+            </footer>
     );
 }
     export default Footer;
