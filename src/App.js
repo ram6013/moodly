a09e57c916f303c57ed4d42b71c902cb801466fc
ram6013 <ramongarciaprado2002@gmail.com>
2024-10-21 12:09:56 +0200

    Inicio Hackaton

--- src/App.js
+++ src/App.js
@@ -4,19 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="bg-black h-screen w-auto">
+        <h1 className='text-white'>Hola</h1>
       </header>
     </div>
   );
