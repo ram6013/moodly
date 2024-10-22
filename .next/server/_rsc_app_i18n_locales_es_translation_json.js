6a9e1b165c00b84f3531d9d61ec1e45ee564a6c0
Alberto Ruiz Fuente <albertoruizfuente@gmail.com>
2024-10-22 14:31:12 +0200

    politicas

---
+++ .next/server/_rsc_app_i18n_locales_es_translation_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_app_i18n_locales_es_translation_json";
+exports.ids = ["_rsc_app_i18n_locales_es_translation_json"];
+exports.modules = {
+
+/***/ "(rsc)/./app/i18n/locales/es/translation.json":
+/*!**********************************************!*\
+  !*** ./app/i18n/locales/es/translation.json ***!
+  \**********************************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"description":"Moodly is an AI-powered application that helps teachers interpret the emotions of children, especially those with learning difficulties or challenges, in real time. Through the analysis of facial expressions and body language, Moodly detects emotions such as confusion, frustration, or joy, providing educators with valuable insights to adjust their teaching and enhance the classroom experience.","tryIt":"Prueba lo!"}');
+
+/***/ })
+
+};
+;
---
+++ .next/server/_ssr_app_i18n_locales_en_translation_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_ssr_app_i18n_locales_en_translation_json";
+exports.ids = ["_ssr_app_i18n_locales_en_translation_json"];
+exports.modules = {
+
+/***/ "(ssr)/./app/i18n/locales/en/translation.json":
+/*!**********************************************!*\
+  !*** ./app/i18n/locales/en/translation.json ***!
+  \**********************************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"description":"Moodly es una aplicación impulsada por IA que ayuda a los profesores a interpretar las emociones de niños, especialmente aquellos con dificultades o problemas de aprendizaje, en tiempo real. A través del análisis de expresiones faciales y lenguaje corporal, Moodly detecta emociones como confusión, frustración o alegría, proporcionando a los educadores insights valiosos para ajustar su enseñanza y mejorar la experiencia en el aula.","tryIt":"Probarlo","contact":"Contactanose","comments":"Comentarios","commentsLarge":"Introduce tu comentario","send":"Enviar"}');
+
+/***/ })
+
+};
+;
